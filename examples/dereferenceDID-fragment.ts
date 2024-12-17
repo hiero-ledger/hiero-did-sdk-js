df6abe30664adb6beb2878a931932e98236e3e17
Jakub Sydor <jakub.sydor@leocode.com>
2024-12-17 10:26:02 +0100

    docs(examples): add examples for dereferencing a did
    Signed-off-by: Jakub Sydor <jakub.sydor@leocode.com>
    Signed-off-by: Jakub Sydor <155525776+sydor-leocode@users.noreply.github.com>

---
+++ examples/dereferenceDID-fragment.ts
@@ -0,0 +1,21 @@
+/*
+ * This example demonstrates how to dereference a DID URL and retrieve
+ * the corresponding verification method using the `dereferenceDID`
+ * function. You can use the `#` fragment to dereference a specific
+ * verification method, verification relationship, or service.
+ */
+import { dereferenceDID } from '@swiss-digital-assets-institute/resolver';
+
+const didUrl =
+  'did:hedera:testnet:23g2MabDNq3KyB7oeH9yYZsJTRVeQ24DqX8o6scB98e3_0.0.5217215#did-root-key';
+
+async function main() {
+  try {
+    const verificationMethod = await dereferenceDID(didUrl);
+    console.log(verificationMethod);
+  } catch (error) {
+    console.error('Error dereferencing DID:', error);
+  }
+}
+
+main();
---
+++ examples/dereferenceDID-service-endpoint.ts
@@ -0,0 +1,21 @@
+/*
+ * This example demonstrates how to dereference a DID URL and retrieve
+ * the corresponding verification method using the `dereferenceDID`
+ * function. You can use the `#` fragment to dereference a specific
+ * verification method, verification relationship, or service.
+ */
+import { dereferenceDID } from '@swiss-digital-assets-institute/resolver';
+
+const didUrl =
+  'did:hedera:testnet:3f3zxTz93CXnqhW3bNxqeyk8Gfk7v2yR27DRgSTYvHog_0.0.5278919?service=github&relativeRef=hashgraph-did-sdk-js';
+
+async function main() {
+  try {
+    const serviceEndpoint = await dereferenceDID(didUrl);
+    console.log(serviceEndpoint);
+  } catch (error) {
+    console.error('Error dereferencing DID:', error);
+  }
+}
+
+main();
