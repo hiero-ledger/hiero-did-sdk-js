6a28ef5ef425fbdfd504eb0597b0beb930d04fcb
Alexander Shenshin <alexander.shenshin@dsr-corporation.com>
2025-10-27 02:45:22 +0300

    Use default import for 'varint' package + enable 'esModuleInterop'
    Signed-off-by: Alexander Shenshin <alexander.shenshin@dsr-corporation.com>

--- packages/core/src/utils/varint-codec.ts
+++ packages/core/src/utils/varint-codec.ts
@@ -1,16 +1,18 @@
-import { encode, decode, encodingLength } from 'varint';
+// Please note that we should not use named exports for 'varint' package as it causes compatibility issues for ESM environments
+// See https://github.com/hiero-ledger/hiero-did-sdk-js/issues/35
+import varint from 'varint';
 
 import { Buffer } from 'buffer';
 
 export class VarintCodec {
   public static decode(data: Uint8Array | number[] | Buffer): readonly [number, number] {
-    const code = decode(data);
-    return [code, decode.bytes] as const;
+    const code = varint.decode(data);
+    return [code, varint.decode.bytes] as const;
   }
 
   public static encode(int: number): Uint8Array {
-    const target = Buffer.alloc(encodingLength(int));
-    encode(int, target);
+    const target = Buffer.alloc(varint.encodingLength(int));
+    varint.encode(int, target);
     return new Uint8Array(target);
   }
 }
