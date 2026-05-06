44171b348531d6bb7fd2cf248805b4a168f776c7
Jayesh Kathale <jayeshkathale24@gmail.com>
2026-05-06 11:17:14 +0530

    fix(client,hcs): replace internal SDK subpath import with public Client export
    Signed-off-by: Jayesh Kathale <jayeshkathale24@gmail.com>

--- packages/hcs/tests/unit/mirror-node.spec.ts
+++ packages/hcs/tests/unit/mirror-node.spec.ts
@@ -1,18 +1,15 @@
-import NodeClient from '@hiero-ledger/sdk/lib/client/NodeClient';
 import { isMirrorQuerySupported } from '../../src/shared';
+import { Client } from '@hiero-ledger/sdk';
 
 describe('isMirrorQuerySupported', () => {
   it('returns true for a NodeClient instance', () => {
-    const client = NodeClient.forTestnet();
-
+    const client = Client.forTestnet();
     expect(isMirrorQuerySupported(client)).toBe(true);
-
     client.close();
   });
 
   it('returns false for a non-NodeClient object', () => {
     const client = Object.create(null);
-
     expect(isMirrorQuerySupported(client)).toBe(false);
   });
 });
